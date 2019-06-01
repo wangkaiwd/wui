45e46f3b07420165223c6c82b6bc1cc33b4a1a19
wangkaiwd <1162486176@qq.com>
2019-06-02 00:26:15 +0800

    feat: 页面第一个文件

---
+++ index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Index extends Component {
+  render () {
+    return (
+      <div>
+        First React Component
+      </div>
+    );
+  }
+}
+
+export default Index;
