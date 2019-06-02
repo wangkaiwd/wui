2e3781280f90411543cad7e72a13c67b7f76d430
wangkaiwd <1162486176@qq.com>
2019-06-02 21:04:24 +0800

    feat: webpack:entry,output,module

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const absPath = dir => path.resolve(__dirname, `./${dir}`);
+module.exports = {
+  entry: {
+    main: './lib/index.tsx'
+  },
+  output: {
+    path: absPath('dist/lib'),
+    library: 'WUI', // bundle文件的名称
+    // Universal Module Definition
+    // universal: 普遍的，通用的
+    // amd,commonjs,umd: https://www.davidbcalhoun.com/2014/what-is-amd-commonjs-and-umd/
+    libraryTarget: 'umd'
+  },
+  // loader: 让webpack能够去处理那些非javascript文件
+  module: {
+    rules: [
+      {
+        // ? : 匹配前面一个表达式0次或者1次。这里可以匹配到.ts或者.tsx文件
+        // $ : 匹配输入的结束
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader'
+      }
+    ]
+  }
+};
