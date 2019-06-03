bcf8bf4aa1260289f5aa4d26ffdedc367f4b1658
wangkaiwd <1162486176@qq.com>
2019-06-03 14:07:32 +0800

    feat: 排除`react`和`react-dom`，因为这些依赖consumer(用户)会自己安装

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,24 @@ module.exports = {
       template: 'index.html'
     }),
   ],
-  performance: {
-    hints: false
+  // external: 外面的，外部的；表面上的
+  // 提供了从输出的bundle中排除依赖的方法。
+  // 官方文档： https://webpack.docschina.org/configuration/externals/#src/components/Sidebar/Sidebar.jsx
+  externals: {
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'react',
+      root: 'React'
+    },
+    'react-dom': {
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'react-dom',
+      root: 'ReactDOM',
+    }
   }
+  // performance: {
+  //   hints: false
+  // }
 };
