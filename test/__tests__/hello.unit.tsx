6d9c30978292586d5f773b44efb0f42ce9e7f4a5
wangkaiwd <1162486176@qq.com>
2019-06-03 17:18:04 +0800

    feat: ts interface进行组件参数的校验

--- test/__tests__/hello.unit.tsx
+++ test/__tests__/hello.unit.tsx
@@ -1,5 +1,9 @@
+function add(a: number, b: number) {
+  return a + b;
+}
+
 describe('我的第一个测试用例', () => {
-  it('1等于1', () => {
-    expect(1).toEqual(1);
+  it('add(1,2)等于3', () => {
+    expect(add(1, 2)).toEqual(3);
   });
 });
