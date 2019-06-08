53099dc7f3f867aea86f7935c8bd46686abb206e
wangkaiwd <1162486176@qq.com>
2019-06-08 20:18:24 +0800

    feat: 将icon组件放入到单独的文件夹中

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,6 +1,6 @@
 import React, {Fragment, MouseEventHandler} from 'react';
 import ReactDOM from 'react-dom';
-import Icon from './icon';
+import Icon from './icon/icon';
 
 const onClick: MouseEventHandler = (e) => {
   console.log('click', e.target);
