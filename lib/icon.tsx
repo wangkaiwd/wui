82b3fefc15f6a0394304d05be6586b56414b8fe2
wangkaiwd <1162486176@qq.com>
2019-06-08 17:24:14 +0800

    feat: 为icon组件添加可选的onClick事件

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import './importAllSvg';
+import './icon.scss';
 
 interface IconProps {
-  name: string
+  name: string,
+  onClick?: React.MouseEventHandler<SVGElement>
 }
 
 const Icon: React.FunctionComponent<IconProps> = (props) => {
   return (
-    <svg>
+    //这里的onClick是js原生的click方法，点击Icon组件，相当于点击组件的根节点，这里通过原生方法触发自定义方法
+    <svg className={'wui-icon'} onClick={props.onClick}>
       <use xlinkHref={`#${props.name}`}/>
     </svg>
   );
