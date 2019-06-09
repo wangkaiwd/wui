e3c8e2ab0692d3afa79ee8d4069e914648152c3f
wangkaiwd <1162486176@qq.com>
2019-06-09 21:02:34 +0800

    feat: 配置devServer来支持BrowserRouter

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,6 +3,8 @@ module.exports = {
   ...base,
   mode: 'development',
   devServer: {
-    contentBase: './dist/lib'
+    contentBase: './dist/lib',
+    // 当使用react-router-dom的BrowserRouter时，任意的404响应都可能需要被替代为index.html。通过下面属性启用
+    // historyApiFallback: true // 这里我们用HashRouter
   },
 };
