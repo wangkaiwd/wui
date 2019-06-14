eb4ff84e70abd3b52cdb4b005928510d253fd97d
wangkaiwd <1162486176@qq.com>
2019-06-14 23:56:58 +0800

    feat: 实现alert功能

--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -1,25 +1,33 @@
 import React, {useState} from 'react';
-import Dialog from './dialog';
+import Dialog, {alert} from './dialog';
 
 const DialogExample: React.FunctionComponent = (props) => {
   const [x, setX] = useState();
   const onClose = () => setX(false);
   return (
     <div>
-      {/*react只能通过自己来更新state的状态，而vue这里可以使用.sync语法糖进行简写*/}
-      <button onClick={() => setX(true)}>click me</button>
-      <Dialog
-        visible={x}
-        onClose={onClose}
-        buttons={
-          [
-            <button onClick={() => setX(false)}>1</button>,
-            <button onClick={() => setX(false)}>2</button>
-          ]
-        }
-      >
-        hi
-      </Dialog>
+      <div>
+        <h3>example 1</h3>
+        {/*react只能通过自己来更新state的状态，而vue这里可以使用.sync语法糖进行简写*/}
+        <button onClick={() => setX(true)}>click me</button>
+        <Dialog
+          visible={x}
+          onClose={onClose}
+          buttons={
+            [
+              <button onClick={() => setX(false)}>1</button>,
+              <button onClick={() => setX(false)}>2</button>
+            ]
+          }
+        >
+          hi
+        </Dialog>
+      </div>
+      <div>
+        <h3>example 2</h3>
+        {/*react只能通过自己来更新state的状态，而vue这里可以使用.sync语法糖进行简写*/}
+        <button onClick={() => alert('1')}>alert me</button>
+      </div>
     </div>
   );
 };
--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -2,7 +2,7 @@ import React, {Fragment} from 'react';
 import {classMaker} from '../helpers/classes';
 import './dialog.scss';
 import Icon from '../icon/icon';
-import {createPortal} from 'react-dom';
+import ReactDOM, {createPortal} from 'react-dom';
 
 interface DialogProps {
   visible: boolean,
@@ -13,7 +13,7 @@ interface DialogProps {
 const scopeClass = classMaker('dialog');
 const sc = scopeClass;
 const Dialog: React.FunctionComponent<DialogProps> = (props) => {
-  const isShowFooter: boolean = (props.buttons && props.buttons.length > 0) ? true : false;
+  const isShowFooter: boolean = !!(props.buttons && props.buttons.length > 0);
   const onClickMask: React.MouseEventHandler = (e) => {
     if (props.maskClosable) {
       props.onClose(e);
@@ -47,6 +47,21 @@ export default Dialog;
 Dialog.defaultProps = {
   maskClosable: true
 };
+const alert = (content: string) => {
+  const onClose = () => {
+    ReactDOM.render(React.cloneElement(component, {visible: false}), div);
+    div.remove();
+    // 官方提供的卸载api，会将事件处理器和state一并清除
+    ReactDOM.unmountComponentAtNode(div);
+  };
+  const component = (
+    <Dialog onClose={onClose} visible={true}>{content}</Dialog>
+  );
+  const div = document.createElement('div');
+  document.body.appendChild(div);
+  ReactDOM.render(component, div);
+};
+export {alert};
 // 以element元素为样板克隆并返回新的React元素。返回元素的props是将新的props与原始元素的props浅层合并后的结果。
 // 新的子元素将取代现有的子元素，而来自原始元素的key和ref将被保留
 // React.cloneElement(element,[props],[...children])
@@ -54,3 +69,7 @@ Dialog.defaultProps = {
 // ReactDOM.createPortal(child,container)：将子节点渲染到存在于父组件以外的DOM节点的优秀的方案
 // 参数1：任何可渲染的React子元素：元素，字符串或Fragment
 // 参数2：DOM元素
+// ------
+// ReactDOM.render(element,container,[,callback])
+// 在提供的container里渲染一个React元素，并返回对该组件的引用
+// 如果React元素之前已经在container里渲染过，这将会对其执行更新操作，并仅会在必要时改变DOM以映射最新的React元素
