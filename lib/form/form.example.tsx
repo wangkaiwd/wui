2c1b21fa89349a433e4daf1341999492428036e1
wangkaiwd <1162486176@qq.com>
2019-06-19 15:30:51 +0800

    feat: form最初步的基本功能实现

--- lib/form/form.example.tsx
+++ lib/form/form.example.tsx
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Form, { FormValue } from './form';
 
-interface Props {
-
-}
-
-const FormExample: React.FC<Props> = (props) => {
+const FormExample: React.FC = (props) => {
+  const [formData, setFormData] = useState<FormValue>({ username: 'wangkaiwd', password: '123456' });
+  const fields = [
+    {
+      key: 'username',
+      label: '用户名',
+      input: { type: 'text' }
+    },
+    {
+      key: 'password',
+      label: '密码',
+      input: { type: 'text' }
+    }
+  ];
+  const onChange = (newValue: FormValue) => {
+    setFormData(newValue);
+  };
+  const onSubmit = () => {
+    console.log('formData', formData);
+    // 将表单中输入的内容作为参数传给后端
+    // props.history.push('/list'): 跳转到列表页
+  };
   return (
-    <div>
-      form
-    </div>
+    <Form
+      formData={formData}
+      fields={fields}
+      onChange={onChange}
+      onSubmit={onSubmit}
+    />
   );
 };
 
--- lib/form/form.tsx
+++ lib/form/form.tsx
@@ -1,14 +1,58 @@
 import React from 'react';
 
-interface Props {
+export interface FormValue {
+  [key: string]: string
+}
 
+interface FieldsProps {
+  key: string,
+  label: string,
+  input: { type: string }
+}
+
+interface Props {
+  formData: FormValue,
+  fields: Array<FieldsProps>,
+  onChange: (newValue: FormValue) => void,
+  onSubmit: () => void
 }
 
 const Form: React.FC<Props> = (props) => {
+  // const onChange = (value: string, key: string) => {
+  //   const newValue = { ...props.formData, [key]: value };
+  //   props.onChange(newValue);
+  // };
+  const onChange = (key: string, e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = { ...props.formData, [key]: e.target.value };
+    props.onChange(newValue);
+  };
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    props.onSubmit();
+  };
+  const onReset = () => {
+    const tempFormData: FormValue = {};
+    for (const k in props.formData) {
+      tempFormData[k] = '';
+    }
+    props.onChange(tempFormData);
+  };
   return (
-    <div>
-      form
-    </div>
+    <form onSubmit={onSubmit} onReset={onReset}>
+      {props.fields.map(f => (
+        <div key={f.key}>
+          {f.label}
+          <input
+            value={props.formData[f.key]}
+            type={f.input.type}
+            // onChange={(e) => onChange(e.target.value, f.key)}
+            onChange={onChange.bind(null, f.key)}
+          />
+        </div>
+      ))}
+      <button type="submit">submit</button>
+      <button type="reset">reset</button>
+    </form>
   );
 };
 
