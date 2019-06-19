280d09748dab2d579156109259bac6bca17f0b7d
wangkaiwd <1162486176@qq.com>
2019-06-19 16:09:01 +0800

    feat: 添加github地址链接

--- lib/codeItem.tsx
+++ lib/codeItem.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import Highlight, { defaultProps } from 'prism-react-renderer';
 import darkTheme from 'prism-react-renderer/themes/duotoneLight';
 // @ts-ignore
-import x from '!!raw-loader!./icon/icon.example';
+import SourceCode from '!!raw-loader!./icon/icon.example';
 
 const CodeItem: React.FC = (props) => {
   return (
-    <Highlight {...defaultProps} theme={darkTheme} code={x} language="jsx">
+    <Highlight {...defaultProps} theme={darkTheme} code={SourceCode} language="jsx">
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
         <pre className={className} style={style}>
           {tokens.map((line, i) => (
--- lib/example.tsx
+++ lib/example.tsx
@@ -11,6 +11,7 @@ import Layout from './layout/layout';
 import Footer from './layout/footer';
 import Sider from './layout/sider';
 import FormExample from './form/form.example';
+import Icon from './icon/icon';
 // 使用require引入文件可以避开ts的检查
 const logo = require('./assets/imgs/logo.png');
 
@@ -19,10 +20,17 @@ const Example: React.FunctionComponent = (props) => {
     <Router>
       <Layout className="example">
         <Header className="example-header">
-          <div className="example-logo">
-            <img src={logo} alt=""/>
+          <div className="example-header-left">
+            <div className="example-logo">
+              <img src={logo} alt=""/>
+            </div>
+            <h3 className="example-name">WUI</h3>
           </div>
-          <h3 className="example-name">WUI</h3>
+          <div className="example-header-right">
+            <a href="https://github.com/wangkaiwd/wui" target="_blank">
+              <Icon className="example-github" name="github"/>
+            </a>
+          </div>
         </Header>
         <Layout className="example-content-wrapper">
           <Sider className="example-sider">
