304fa5925c386e4da2c187c287747bf8af73bb9e
wangkaiwd <1162486176@qq.com>
2019-07-25 11:51:08 +0800

    feat: 优化代码高亮的样式

--- lib/codeItem.tsx
+++ lib/codeItem.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import Highlight, { defaultProps } from 'prism-react-renderer';
-import lightTheme from 'prism-react-renderer/themes/duotoneLight';
+// import lightTheme from 'prism-react-renderer/themes/oceanicNext';
 import { classMaker } from './helpers/classes';
 import './codeItem.scss';
 
 const sc = classMaker('code-item');
+
 interface Props {
   code: string,
   title?: string
 }
+
 const CodeItem: React.FC<Props> = (props) => {
   return (
     <div className={sc()}>
@@ -16,17 +18,17 @@ const CodeItem: React.FC<Props> = (props) => {
         {props.children}
       </div>
       <div className={sc('code')}>
-        <Highlight {...defaultProps} theme={lightTheme} code={props.code} language="jsx">
+        <Highlight {...defaultProps} theme={undefined} code={props.code} language="jsx">
           {({ className, style, tokens, getLineProps, getTokenProps }) => (
             <pre className={className} style={style}>
-          {tokens.map((line, i) => (
-            <div {...getLineProps({ line, key: i })}>
-              {line.map((token, key) => (
-                <span {...getTokenProps({ token, key })} />
-              ))}
-            </div>
-          ))}
-          </pre>
+            {tokens.map((line, i) => (
+              <div {...getLineProps({ line, key: i })}>
+                {line.map((token, key) => (
+                  <span {...getTokenProps({ token, key })} />
+                ))}
+              </div>
+            ))}
+            </pre>
           )}
         </Highlight>
       </div>
