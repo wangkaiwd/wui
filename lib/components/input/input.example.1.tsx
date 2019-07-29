d135e64df82eda6037b1c3e95804fb8f900d92dc
wangkaiwd <1162486176@qq.com>
2019-07-29 11:37:31 +0800

    feat: input错误信息展示完成

--- lib/components/input/input.example.1.tsx
+++ lib/components/input/input.example.1.tsx
@@ -7,6 +7,7 @@ const InputExample1 = () => {
       <Input className={'separate'} placeholder={'search...'}/>
       <Input className={'separate'} placeholder={'search...'} prefix={'qq'}/>
       <Input className={'separate'} placeholder={'search...'} suffix={'alipay'}/>
+      <Input className={'separate'} placeholder={'search...'} error={'这是错误信息'}/>
     </Fragment>
   );
 };
